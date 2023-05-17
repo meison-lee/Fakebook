b8df030ef437c86d190119790f987de5ff2c08c4
Meison <b08505012@ntu.edu.tw>
2023-05-17 11:00:39 +0800

    update

--- backend/mongo.js
+++ backend/mongo.js
@@ -11,6 +11,7 @@ const connectMongoDB = () => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
+
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function () {
