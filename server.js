3f2a4e9f5ba37d60182530ac199ffa22b01c016a
Meison <b08505012@ntu.edu.tw>
2023-06-08 20:02:43 +0800

    eks_yaml_file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import db from './backend/models.js';
 import { resolvers } from './backend/resolvers.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 const typeDefs = importSchema("./backend/schema.graphql");
 const pubSub = new PubSub();
